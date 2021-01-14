92f8dcca040b80a6a86b85f18ccaf4a0ecf0bf91
Phillip <phillip.mittmann@gmail.com>
2021-01-14 17:28:30 -0300

    Style do componente

--- src/screens/FiltrarAtividades.js
+++ src/screens/FiltrarAtividades.js
@@ -6,19 +6,34 @@ import {
     View
 } from 'react-native';
 
-import { Picker } from '@react-native-picker/picker';
 import Button from '../components/Button';
+
+import { Picker } from '@react-native-picker/picker';
 
 import firestore from '@react-native-firebase/firestore';
 
 const FiltrarAtividades = () => {
     return (
+        <View style={ styles.container }>
+            <View style={ styles.containerPicker }>
 
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+    containerPicker: {
+        height: '30%',
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginVertical: 20
+    },
 });
 
 export default FiltrarAtividades;
